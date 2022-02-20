b151b00f7f44fdb1902d2b956678d16cb142798a
dwikyfahlavi <dwikyfahlavifahlavi.dwiky@upi.edu>
2022-02-20 12:21:33 +0700

    add praktikum

---
+++ 05_Basic Dart Programming/praktikum/bin/praktikum.dart
@@ -0,0 +1,14 @@
+void main() {
+  //soal no 1
+  final phi = 3.14;
+  var r = 10;
+  var luas = phi * r * r;
+  print(luas);
+
+  //soal no 2
+  final name = 'Nama aku adalah';
+  var first_name = 'Dwiqy Fahlavi';
+  var last_name = 'Muhammad Ramdani';
+  var full_name = name + " " + first_name + " " + last_name;
+  print(full_name);
+}
