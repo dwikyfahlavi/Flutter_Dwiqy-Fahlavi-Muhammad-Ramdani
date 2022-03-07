e27a30aa323764199ba93b93b236920d65cb6d91
dwikyfahlavi <dwikyfahlavifahlavi.dwiky@upi.edu>
2022-03-07 19:20:04 +0700

    resolve bugs

--- 09_Dart Object Oriented Programming 1/praktikum/task.dart
+++ 09_Dart Object Oriented Programming 1/praktikum/task.dart
@@ -14,7 +14,7 @@ void main() {
   var kucing = new Hewan();
   kucing.berat = 5;
   var kucing2 = new Hewan();
-  kucing.berat = 10;
+  kucing2.berat = 10;
   var truck = Mobil();
   truck.tambahMuatan(kucing);
   truck.tambahMuatan(kucing2);
