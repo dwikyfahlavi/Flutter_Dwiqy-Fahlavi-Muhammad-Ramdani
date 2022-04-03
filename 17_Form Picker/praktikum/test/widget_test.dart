fcfefbc5cbff3928412f2463b11ef84d8672ac04
dwikyfahlavi <dwikyfahlavifahlavi.dwiky@upi.edu>
2022-04-03 22:25:16 +0700

    z

--- 17_Form Picker/praktikum/test/widget_test.dart
+++ 17_Form Picker/praktikum/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:praktikum/main1.dart';
+import 'package:praktikum/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
