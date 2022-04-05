7e2164a023c1a2ed55ed08f0083c887d59bba3b1
dwikyfahlavi <dwikyfahlavifahlavi.dwiky@upi.edu>
2022-04-05 20:16:24 +0700

    add praktikum

---
+++ 20_Flutter State Management (Provider)/praktikum/lib/models/contact_model.dart
@@ -0,0 +1,16 @@
+class ContactModel {
+  final String name;
+  final String number;
+  final String avatar;
+  final String status;
+  final String gender;
+  final String hobi;
+
+  ContactModel(
+      {required this.name,
+      required this.number,
+      required this.avatar,
+      required this.status,
+      required this.gender,
+      required this.hobi});
+}
